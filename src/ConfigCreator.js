70eacc2ea9c142357de77375c7b6f84f862344e9
apennamen <adrien.pennamen@soprasteria.com>
2020-05-09 00:40:59 +0200

    feat: add ColorSelector component

--- src/ConfigCreator.js
+++ src/ConfigCreator.js
@@ -1,8 +1,18 @@
 import { LitElement, html, css } from 'lit-element';
-import rough from 'roughjs';
 import 'wired-input';
 import 'wired-combo';
 import 'wired-item';
+
+import { ColorSelector } from './ColorSelector';
+customElements.define('color-selector', ColorSelector);
+
+const DEFAULT_CONFIG = {
+    fill: '#fff',
+    fillStyle: 'zigzag',
+    fillWeight: 1.5,
+    roughness: 0.1,
+    stroke: '#000',
+};
 
 export class ConfigCreator extends LitElement {
 
@@ -10,72 +20,65 @@ export class ConfigCreator extends LitElement {
         return css`
             :host {
                 display: flex;
-                flex-direction:row;
-                align-items: center;
+                flex-direction:column;
+                align-items: flex-end;
                 font-size: 2vmin;
                 font-family: inherit;
-                --wired-item-selected-bg: #90C1B3;
+                --wired-item-selected-bg: var(--primary-color);
             }
-
-            wired-input {
-                font-family: inherit;
-                width: 5em;
+            wired-combo {
+                right: -10px;
             }
         `;
     }
 
     get config() {
-        return this._config;
+        return {...this._config};
     }
 
     constructor() {
         super();
-        this._config = {fill: '#90C1B3', fillStyle: 'zigzag', fillWeight: 1.5, roughness: 0.1};
+        DEFAULT_CONFIG.fill = getComputedStyle(document.documentElement)
+            .getPropertyValue('--primary-color').trim();
+        this._config = { ...DEFAULT_CONFIG };
     }
 
     render() {
-        const handleColorChange = (e) => {
-            const color = e.detail.sourceEvent.target.value;
-            const regex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
-            if (regex.test(color)) {
-                this._config = {...this._config, fill: color};
-                this.updateCanvas();
-            }
+        const handleFillColorChange = (e) => {
+            this._config = {...this._config, fill: e.detail.color};
+        }
+        const handleStrokeColorChange = (e) => {
+            this._config = {...this._config, stroke: e.detail.color};
         }
         const handleFillStyleChange = (e) => {
             this._config = {...this._config, fillStyle: e.detail.selected};
-            this.updateCanvas();
         }
 
         return html`
-        <wired-combo
-            id="fillStyleCombo"
-            .selected=${this.config.fillStyle}
-            @selected=${handleFillStyleChange}
-        >
-            <wired-item value="zigzag">ZigZag</wired-item>
-            <wired-item value="solid">Solid</wired-item>
-            <wired-item value="hachure">Hachure</wired-item>
-            <wired-item value="cross-hatch">Cross-Hatch</wired-item>
-            <wired-item value="dots">Dots</wired-item>
-            <wired-item value="dashed">Dashed</wired-item>
-        </wired-combo>
-        <wired-input @input=${handleColorChange} placeholder="#90C1B3"></wired-input>
-        <canvas height="45" width="30"></canvas>
+        <div>Fill style</span>
+            <wired-combo
+                id="fillStyleCombo"
+                .selected=${this.config.fillStyle}
+                @selected=${handleFillStyleChange}
+            >
+                <wired-item value="zigzag">ZigZag</wired-item>
+                <wired-item value="solid">Solid</wired-item>
+                <wired-item value="hachure">Hachure</wired-item>
+                <wired-item value="cross-hatch">Cross-Hatch</wired-item>
+                <wired-item value="dots">Dots</wired-item>
+                <wired-item value="dashed">Dashed</wired-item>
+            </wired-combo>
+        </div>
+        <color-selector
+            color="${DEFAULT_CONFIG.fill}"
+            label="Fill color"
+            @colorchange=${handleFillColorChange}>
+        </color-selector>
+        <color-selector
+            color="${DEFAULT_CONFIG.stroke}"
+            label="Stroke color"
+            @colorchange=${handleStrokeColorChange}>
+        </color-selector>
     `;
-    }
-
-    firstUpdated() {
-        this.updateCanvas();
-    }
-
-    /**
-     * Call this function when this._config is changed;
-     */
-    updateCanvas() {
-        const canvas = this.renderRoot.querySelector('canvas');
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-        const rc = rough.canvas(canvas);
-        rc.rectangle(0, 0, canvas.width, canvas.height, this.config);
     }
 }
