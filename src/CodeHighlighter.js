7f05b539f99b9c2b4917d2284cc4ff2df845bd24
apennamen <adrien.pennamen@soprasteria.com>
2020-05-08 23:04:29 +0200

    feat: code highlighter component

---
+++ src/CodeHighlighter.js
@@ -0,0 +1,72 @@
+import { LitElement, html, css } from 'lit-element';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
+import hljs from 'highlight.js';
+import xml from 'highlight.js/lib/languages/xml';
+
+import { highlightCss } from './highlightCss';
+
+export class CodeHighlighter extends LitElement {
+
+    static get properties() {
+        return {
+            code: { type: String },
+        }
+    }
+
+    static get styles() {
+        return [
+            highlightCss,
+            css`
+              :host {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+              }
+              wired-button {
+                background: var(--primary-color);
+              }
+              code::-webkit-scrollbar {
+                width: 30px;
+              }
+              code {
+                scrollbar-width: thin;
+                scrollbar-color: var(--thumbBG) var(--scrollbarBG);
+                font-size: 2vmin;
+                text-align: left;
+                max-width: 60vw;
+              }
+              code::-webkit-scrollbar-track {
+                background: var(--scrollbarBG);
+              }
+              code::-webkit-scrollbar-thumb {
+                background-color: var(--thumbBG) ;
+                border-radius: 6px;
+                border: 1px solid var(--scrollbarBG);
+              }
+            `,
+        ];
+    }
+
+    cosntructor() {
+        this.code='';
+        hljs.registerLanguage('xml', xml);
+    }
+
+    render() {
+        const handleCopy = () => {
+            var dummy = document.createElement("textarea");
+            this.shadowRoot.appendChild(dummy);
+            dummy.value = this.code.trim();
+            dummy.select();
+            document.execCommand("copy");
+            this.shadowRoot.removeChild(dummy);
+        }
+        
+        const highlightedCode = hljs.highlight('xml', this.code.trim());
+        
+        return html`
+            <pre><code class="xml hljs">${unsafeHTML(highlightedCode.value)}</code></pre>
+            <wired-button style="margin-left: 10px" elevation="2" @click=${handleCopy}>Copy</wired-button>
+        `;
+    }
+}
--- src/WiredIconGenerator.js
+++ src/WiredIconGenerator.js
@@ -1,28 +1,19 @@
 import { LitElement, html, css } from 'lit-element';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
-import hljs from 'highlight.js';
-import xml from 'highlight.js/lib/languages/xml'
 import 'wired-textarea';
 import 'wired-button';
 
-hljs.registerLanguage('xml', xml);
-
 import { ExtWiredTextarea } from './ExtWiredTextarea';
 import { ConfigCreator } from './ConfigCreator';
+import { CodeHighlighter } from './CodeHighlighter';
 import { wiredSvg } from './wiredSvg';
 import { highlightCss } from './highlightCss';
 
 customElements.define('x-wired-textarea', ExtWiredTextarea);
 customElements.define('config-creator', ConfigCreator);
+customElements.define('code-highlighter', CodeHighlighter);
 
 export class WiredIconGenerator extends LitElement {
-  static get properties() {
-    return {
-      title: { type: String },
-      page: { type: String },
-    };
-  }
-
   static get styles() {
     return [
       highlightCss,
@@ -31,24 +22,9 @@ export class WiredIconGenerator extends LitElement {
         --scrollbarBG: #CFD8DC;
         --thumbBG: #1a2b42;
       }
-      code::-webkit-scrollbar {
-        width: 30px;
-      }
-      code {
-        scrollbar-width: thin;
-        scrollbar-color: var(--thumbBG) var(--scrollbarBG);
-      }
-      code::-webkit-scrollbar-track {
-        background: var(--scrollbarBG);
-      }
-      code::-webkit-scrollbar-thumb {
-        background-color: var(--thumbBG) ;
-        border-radius: 6px;
-        border: 1px solid var(--scrollbarBG);
-      }
 
       wired-button {
-        background: #90C1B3;
+        background: var(--primary-color);
       }
 
       .config,
@@ -74,26 +50,15 @@ export class WiredIconGenerator extends LitElement {
         justify-content: center;
       }
 
-      code {
-        font-size: 2vmin;
-        text-align: left;
-        max-width: 60vw;
-      }
-
       #svg {
         border: 2px dashed #1a2b42;
         width: 80px;
         height: 80px;
       }
+
       #svg svg {
         width: 80px;
         max-height: 80px;
-      }
-
-      .result {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
       }
     `];
   }
@@ -106,11 +71,6 @@ export class WiredIconGenerator extends LitElement {
   <text>4. Grab the converted svg code here!</text>
 </svg>
     `;
-    this.highlightedCode = hljs.highlight('xml', this.outputSvg.trim());
-  }
-
-  firstUpdated() {
-    this.dispatchEvent(new CustomEvent('loaded', {bubbles: true, composed: true}));
   }
 
   render() {
@@ -118,45 +78,33 @@ export class WiredIconGenerator extends LitElement {
 
 <svg viewbox="0 0 16 16"><path d="M9.5 14c0 1.105-0.895 2-2 2s-2-0.895-2-2c0-1.105 0.895-2 2-2s2 0.895 2 2z"></path></svg>
     `;
-    const handleRefresh = () => {
+    const handleLoad = () => {
       const textarea = this.renderRoot.querySelector('#input');
       const svgCopy = this.renderRoot.querySelector('#svg');
       svgCopy.innerHTML = textarea.value;
       this.inputSvg = textarea.value;
-      this.requestUpdate();
+      return this.requestUpdate();
     }
-    const handleConvert = () => {
+    const handleConvert = async () => {
+      await handleLoad();
       const svgToConvert = this.renderRoot.querySelector('#svg').firstElementChild;
       if (!svgToConvert) return;
       const { config } = this.renderRoot.querySelector('config-creator');
       wiredSvg(svgToConvert, config);
       this.outputSvg = svgToConvert.outerHTML;
-      this.highlightedCode = hljs.highlight('xml', this.outputSvg);
-      this.requestUpdate();
-    }
-    const handleCopy = () => {
-      var dummy = document.createElement("textarea");
-      document.body.appendChild(dummy);
-      dummy.value = this.outputSvg;
-      dummy.select();
-      document.execCommand("copy");
-      document.body.removeChild(dummy);
+      return this.requestUpdate();
     }
     return html`
         <div class="loading">
           <x-wired-textarea id="input" rows="8" .placeholder=${placeholder}></x-wired-textarea>
-          <wired-button elevation="2" @click=${handleRefresh}>2. Load It</wired-button>
+          <wired-button elevation="2" @click=${handleLoad}>2. Load It</wired-button>
         </div>
         <div class="config">
           <div id="svg">${unsafeHTML(this.inputSvg)}</div>
           <config-creator></config-creator>
           <wired-button elevation="2" @click=${handleConvert}>3. Convert</wired-button>
         </div>
-
-        <div class="result">
-          <pre><code class="xml hljs">${unsafeHTML(this.highlightedCode.value)}</code></pre>
-          <wired-button style="margin-left: 10px" elevation="2" @click=${handleCopy}>Copy</wired-button>
-        </div>
+        <code-highlighter .code=${this.outputSvg.trim()}></code-highlighter>
     `;
   }
 }
